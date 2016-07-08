0150a66637f2389a77c7c6afadafcaadfca2d277
张玉琳 <2489318084@qq.com>
2016-07-08 09:13:04 +0800

    Initial Commit

---
+++ IkkyuChegj/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IkkyuChegj
+//
+//  Created by 石家庄盛航 on 16/7/8.
+//  Copyright © 2016年 sjzshtx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
